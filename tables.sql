c2d8803fa59388c5181a6d9652da2e7e3f7ed7e5
Maksym Zhukov <44530427+MaksymZhukov@users.noreply.github.com>
2020-04-16 19:55:21 +0900

    Create tables.sql

---
+++ tables.sql
@@ -0,0 +1,53 @@
+CREATE TABLE [students] (
+	student_id int NOT NULL,
+	name char(255) NOT NULL,
+	lastname char(255) NOT NULL,
+  CONSTRAINT [PK_STUDENTS] PRIMARY KEY CLUSTERED
+  (
+  [student_id] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+CREATE TABLE [subjects] (
+	subject_id int NOT NULL,
+	title char(255) NOT NULL,
+  CONSTRAINT [PK_SUBJECTS] PRIMARY KEY CLUSTERED
+  (
+  [subject_id] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+CREATE TABLE [activities] (
+	act_id int NOT NULL,
+	title char(255) NOT NULL,
+  CONSTRAINT [PK_ACTIVITIES] PRIMARY KEY CLUSTERED
+  (
+  [act_id] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+CREATE TABLE [act_scores] (
+	act_score_id int NOT NULL,
+	student_id int NOT NULL,
+	subject_id int NOT NULL,
+	act_id int NOT NULL,
+	score int NOT NULL,
+	module int NOT NULL,
+	semester int NOT NULL,
+  CONSTRAINT [PK_ACT_SCORES] PRIMARY KEY CLUSTERED
+  (
+  [act_score_id] ASC
+  ) WITH (IGNORE_DUP_KEY = OFF)
+
+)
+GO
+CREATE TABLE [final_scores] (
+	score_id int NOT NULL,
+	student_id int NOT NULL,
+	subject_id int NOT NULL,
+	score int NOT NULL
+)
+GO
